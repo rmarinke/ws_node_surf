622b2ea84d082a14fa64f76ff2c7a5fe59c640ea
Marinke <rmarinke@gmail.com>
2023-04-16 23:39:55 -0300

    step 6

--- test/functional/globals.d.ts
+++ test/functional/globals.d.ts
@@ -7,7 +7,7 @@
 // }
 
 declare global {
-    var testRequest: import('supertest').SuperTest<import('supertest').Test>;
+  var testRequest: import('supertest').SuperTest<import('supertest').Test>;
 }
-  
+
 export {};
--- test/jest-setup.ts
+++ test/jest-setup.ts
@@ -1,10 +1,8 @@
-import { SetupServer } from "@src/server";
-import supertest from "supertest";
+import { SetupServer } from '@src/server';
+import supertest from 'supertest';
 
-beforeAll(()=>{
-    const server = new SetupServer();
-    server.init();
-    global.testRequest = supertest(server.getApp());
-
-
-})
+beforeAll(() => {
+  const server = new SetupServer();
+  server.init();
+  global.testRequest = supertest(server.getApp());
+});
